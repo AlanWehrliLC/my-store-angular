67afdfb3a3ed0a615989b2cce25222d2eb29e277
AlanWehrliLC <wehrlialan1@gmail.com>
2022-12-08 13:08:47 -0300

    View product details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-details.component.less']
 })
 export class ProductDetailsComponent {
+  product: Product | undefined
 
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(){
+    const routerParams = this.route.snapshot.paramMap
+    const productIdFromRoute = Number(routerParams.get('productId'))
+
+    this.product = products.find(product => product.id === productIdFromRoute)
+  }
 }
